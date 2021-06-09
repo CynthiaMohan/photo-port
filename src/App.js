5f0b83f1251f4d7ca593d094fc013ca5ca1b89aa
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-09 14:29:44 -0700

    hardcoded values to check the gallery component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import About from './components/About';
+import Gallery from './components/Gallery';
 import Nav from './components/Nav';
+
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
