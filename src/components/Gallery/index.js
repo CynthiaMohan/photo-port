be5807237eee2191ed37c7d229cfff713c4798fd
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-09 14:59:44 -0700

    updated state to nav but have errors

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 import photo from '../../assets/small/commercial/0.jpg';
 
+// Note : 
+//useState() is a function that will always return an array.
+//The first item is the value of your state, and the second item is a setter, which allows you to set the state to something else. 
+
+
 function Gallery() {
     const currentCategory = {
         name: "commercial",
