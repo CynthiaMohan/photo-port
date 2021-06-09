f53ef2a80166aea562b5d243138654825a82db31
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-09 15:03:25 -0700

    component PhotoList created

---
+++ src/components/PhotoList/index.js
@@ -0,0 +1,13 @@
+import photo from "../../assets/small/commercial/0.jpg";
+
+function PhotoList() {
+    return (
+        <div>
+            <img
+                src={photo}
+                alt="Commercial Example"
+            />
+        </div>
+    );
+}
+export default PhotoList;
