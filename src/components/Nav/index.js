f3f65b31992acf05a6a986c4055743dae729b18f
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-09 02:40:53 -0700

    nav test fails

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,7 +13,7 @@ function categorySelected() {
 
 function Nav() {
     return (
-        <header>
+        <header className="flex-row px-1">
             <h2>
                 <a href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
