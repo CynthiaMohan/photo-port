03c9a53faeb7f8be983555d6dd17a5a394ceb5d4
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-15 01:42:03 -0700

    updates

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -3,6 +3,7 @@ import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Gallery(props) {
+
   const { currentCategory } = props;
   return (
     <section>
